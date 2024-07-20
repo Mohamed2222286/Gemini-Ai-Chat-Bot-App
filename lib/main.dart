310313bfd37d0ba6f335a819fe04633666098db3
Mohamed2222286 <mohamedabdelkareem531@gmail.com>
2024-07-20 07:04:07 +0300

    My Second Project

---
+++ lib/main.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+
+const String _apikey = 'AIzaSyCDjaawuJTVZE1awr7h2GRAyaZM2oLyr8g';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  late final GenerativeModel _model;
+  late final ChatSession _chat;
+  final ScrollController _scrollController = ScrollController();
+  final TextEditingController _textEditingController = TextEditingController();
+  final List<ChatMessage> _messsage = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _model = GenerativeModel(model: 'gemini-1.5-flash', apiKey: _apikey);
+    _chat = _model.startChat();
+  }
+
+  void _scrollDown() {
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) => _scrollController.animateTo(
+        _scrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOutCirc,
+      ),
+    );
+  }
+
+  Future<void> _sendChatMessage(String message) async {
+    setState(() {
+      _messsage.add(ChatMessage(text: message, isUser: true));
+    });
+    try {
+      final response = await _chat.sendMessage(Content.text(message));
+      final text = response.text;
+      setState(() {
+        _messsage.add(ChatMessage(text: text!, isUser: false));
+        _scrollDown();
+      });
+    } catch (e) {
+      setState(() {
+        _messsage.add(ChatMessage(text: 'Error Occured', isUser: false));
+      });
+    } finally {
+      _textEditingController.clear();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF15202B),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF15202B),
+        title: const Text(
+          'Ai ChatBot',
+          style: TextStyle(
+              fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              controller: _scrollController,
+              itemCount: _messsage.length,
+              itemBuilder: (context, index) {
+                return ChatBubble(message: _messsage[index]);
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700,
+                    ),
+                    onSubmitted: _sendChatMessage,
+                    controller: _textEditingController,
+                    decoration: InputDecoration(
+                      hintText: ' Message ChatBot',
+                      hintStyle: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(30)),
+                  child: IconButton(
+                    onPressed: () =>
+                        _sendChatMessage(_textEditingController.text),
+                    icon: const Icon(
+                      Icons.send,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ChatMessage {
+  final String text;
+  final bool isUser;
+
+  ChatMessage({required this.text, required this.isUser});
+}
+
+class ChatBubble extends StatelessWidget {
+  final ChatMessage message;
+
+  const ChatBubble({super.key, required this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+      alignment: message.isUser ? Alignment.centerRight : Alignment.centerLeft,
+      child: Container(
+        constraints: BoxConstraints(
+          maxWidth: MediaQuery.of(context).size.width / 1.25,
+        ),
+        padding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 14,
+        ),
+        decoration: BoxDecoration(
+            color: message.isUser ? Colors.blue[200] : Colors.green[200],
+            borderRadius: BorderRadius.only(
+              topLeft: const Radius.circular(12),
+              topRight: const Radius.circular(12),
+              bottomLeft:
+                  message.isUser ? const Radius.circular(12) : Radius.zero,
+              bottomRight:
+                  message.isUser ? Radius.zero : const Radius.circular(12),
+            )),
+        child: Text(
+          message.text,
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
